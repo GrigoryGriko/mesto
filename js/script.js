0f61a5059f409037e7e2dd6e8f1188230f30de83
Grigory Griko <griko1996@gmai.com>
2022-05-16 22:55:11 +0300

    feat: доделан функционал js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 let page = document.querySelector('.page');
 let buttonEdit = page.querySelector('.profile__button-edit');
 let popup = page.querySelector('.popup');
+let popupContainer = page.querySelector('.popup__container');
 let buttonClose = popup.querySelector('.popup__button-close');
 let buttonSave = popup.querySelector('.popup__button-save');
 
 let inputName = popup.querySelector('.popup__name-input');
 let inputJob = popup.querySelector('.popup__job-input');
 
+let textName = page.querySelector('.profile__info-name');
+let textJob = page.querySelector('.profile__info-descript');
 
 function setInputValue() {
-  let textName = page.querySelector('.profile__info-name');
-  let textJob = page.querySelector('.profile__info-descript');
-
-
   inputName.value = textName.textContent;
   inputJob.value = textJob.textContent;
 }
@@ -24,10 +23,18 @@ function formClose(e) {
   e.preventDefault();
   popup.classList.remove('popup_opened');
 }
+function formSubmitSave(e) {
+  e.preventDefault();
+  textName.textContent = inputName.value;
+  textJob.textContent = inputJob.value;
+  popup.classList.remove('popup_opened');
+}
 
 
 setInputValue();
 buttonEdit.addEventListener('click', formEdit);
 buttonClose.addEventListener('click', formClose);
+buttonSave.addEventListener('click', formSubmitSave);
+popupContainer.addEventListener('submit', formSubmitSave);
 
 
