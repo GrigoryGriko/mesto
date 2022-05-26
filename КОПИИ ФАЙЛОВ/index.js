2b7953bf11c9db6ec0e957e6fe91bb32b085cc7d
GrigoryGriko <griko1996@gmail.com>
2022-05-26 09:17:27 +0300

    feat: оптимизировано закрытие popup

--- КОПИИ ФАЙЛОВ/index.js
+++ КОПИИ ФАЙЛОВ/index.js
@@ -8,52 +8,20 @@ const popupAddForm = page.querySelector('#add-form');
 
 const popupContainer = page.querySelector('.popup__container');
 const buttonClose = page.querySelectorAll('.popup__button-close');
-const buttonSave = page.querySelector('.popup__button-save');
+const buttonSave = page.querySelector('#button-save-data');
+const buttonAddCard = page.querySelector('#button-add-card');
 
-let inputName = page.querySelector('.popup__name-input');
-let inputJob = page.querySelector('.popup__job-input');
+let NameInput = page.querySelector('#name-input');
+let JobInput = page.querySelector('#job-input');
+
+let CardNameInput = page.querySelector('#card-name-input');
+let CardLinkInput = page.querySelector('#card-link-input');
 
 let textName = page.querySelector('.profile__info-name');
 let textJob = page.querySelector('.profile__info-descript');
 
-function setInputValue() {
-  inputName.value = textName.textContent;
-  inputJob.value = textJob.textContent;
-}
-
-function openForm(popupId) {
-  popupId.classList.add('popup_opened');
-  setInputValue();
-}
-function closeForm(i) {
-  popup[i].classList.remove('popup_opened');
-}
-function SubmitSaveForm(e) {
-  e.preventDefault();
-  textName.textContent = inputName.value;
-  textJob.textContent = inputJob.value;
-  closeForm();
-}
-
-
-buttonEdit.addEventListener( 'click', function() {
-  openForm(popupEditForm);
-}, false);
-buttonAdd.addEventListener( 'click', function() {
-  openForm(popupAddForm);
-}, false);
-
-
-buttonClose[0].addEventListener('click', closeForm);
-buttonClose[1].addEventListener('click', closeForm);
-
-buttonSave.addEventListener('click', SubmitSaveForm);
-
-
-
 
 const elementsGrid = page.querySelector('.elements-grid__list');
-
 const cardsList = [
   {
     name: 'Архыз',
@@ -81,23 +49,77 @@ const cardsList = [
   }
 ];
 
+function addCardToList(itemName, itemLink) {
+  elementsGrid.insertAdjacentHTML('afterbegin', `
+  <li class="elements-grid__item">
+    <img class="elements-grid__image" src="${itemLink}" alt="${itemName}">
+
+    <div class="elements-grid__text-like-wrapper">
+      <h2 class="elements-grid__place-name">
+        ${itemName}
+      </h2>
+      <button class="elements-grid__like" type="button"></button>
+    </div>
+
+  </li>
+  `);
+}
 function initialCards(cardsList=[]) {
   cardsList.forEach(function (item) {
-    elementsGrid.insertAdjacentHTML('afterbegin', `
-    <li class="elements-grid__item">
-      <img class="elements-grid__image" src="${item.link}" alt="${item.name}">
-
-      <div class="elements-grid__text-like-wrapper">
-        <h2 class="elements-grid__place-name">
-          ${item.name}
-        </h2>
-        <button class="elements-grid__like" type="button"></button>
-      </div>
-
-    </li>
-    `);
+    addCardToList(item.name, item.link);
   })
 }
 
-
 initialCards(cardsList);
+
+
+function setInputValue() {
+  NameInput.value = textName.textContent;
+  JobInput.value = textJob.textContent;
+}
+
+function openForm(popupId) {
+  popupId.classList.add('popup_opened');
+  setInputValue();
+}
+
+
+function closePopup(nameButton, action='default') {
+  if (action === 'default') {
+    nameButton.addEventListener('click', function() {
+      (nameButton.parentElement.parentElement.parentElement).classList.remove('popup_opened');
+    });
+  } else {
+    nameButton.addEventListener('click', function() {
+      (nameButton.parentElement.parentElement).classList.remove('popup_opened');
+    });
+  }
+}
+
+function SubmitSaveForm(e) {
+  e.preventDefault();
+  textName.textContent = NameInput.value;
+  textJob.textContent = JobInput.value;
+  closePopup(buttonSave);
+}
+
+function SubmitAddCard(e) {
+  e.preventDefault();
+  addCardToList(CardNameInput.value, CardLinkInput.value);
+  closePopup(buttonAddCard);
+}
+
+buttonSave.addEventListener('click', SubmitSaveForm);
+buttonAddCard.addEventListener('click', SubmitAddCard);
+closePopup(buttonClose[0], 'close');
+closePopup(buttonClose[1], 'close');
+
+
+function openFormClick(buttonName, formName) {
+  buttonName.addEventListener( 'click', function() {
+    openForm(formName);
+  }, false);
+}
+
+openFormClick(buttonEdit, popupEditForm);
+openFormClick(buttonAdd, popupAddForm);
