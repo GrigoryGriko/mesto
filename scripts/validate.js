69748a400dd7c8e21e1520c443c7736a4cebe369
Grigory Griko <griko1996@gmai.com>
2022-06-05 22:33:23 +0300

    style: стиль для блоков с сообщением о валидации

---
+++ scripts/validate.js
@@ -0,0 +1,18 @@
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
+enableValidation (elements) {
+  /*
+  получаем форму
+  получаем поле формы
+  получаем поле с сообщением о валидации
+
+
+  */
+}
