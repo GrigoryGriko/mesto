1dcb169ffc6d6d21d373938f5e41533ece3973dc
GrigoryGriko <griko1996@gmail.com>
2022-06-06 10:17:01 +0300

    feat: написание сообщения ошибки валидации

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,18 +1,20 @@
 enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
+  formSelector: '.popup__container',
+  inputSelector: '.input-general-properties',
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 });
 
-enableValidation (elements) {
-  /*
-  получаем форму
-  получаем поле формы
-  получаем поле с сообщением о валидации
+function enableValidation(elementsDocument) {
+  const form = document.querySelector(elementsDocument.formSelector);
+  const formInput = form.querySelector(elementsDocument.inputSelector);
+  const formError = form.querySelector(`.${formInput.id}-error`);
+
+  function showInputError(inputElement, errorMessage) {
+    inputElement.classList.add(elementsDocument.inputErrorClass);
 
 
-  */
+  }
 }
